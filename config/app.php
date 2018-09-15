5502fa4542895ad54e2a41d8bae73dd898931e30
Jamila Ferron <jamila_ferron@hotmail.co.uk>
2018-09-15 19:03:12 +0100

    api4

--- config/app.php
+++ config/app.php
@@ -160,6 +160,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // ...
+        Collective\Html\HtmlServiceProvider::class,
+        MaddHatter\LaravelFullcalendar\ServiceProvider::class,
+        Unicodeveloper\DumbPassword\DumbPasswordServiceProvider::class
+
     ],
 
     /*

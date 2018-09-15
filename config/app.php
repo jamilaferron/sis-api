c6611583ee299ef83f421a3adc0fc2ecd1f8d0a4
Jamila Ferron <jamila_ferron@hotmail.co.uk>
2018-09-15 19:14:42 +0100

    api4

--- config/app.php
+++ config/app.php
@@ -161,8 +161,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         // ...
-       
-        MaddHatter\LaravelFullcalendar\ServiceProvider::class,
+
         Unicodeveloper\DumbPassword\DumbPasswordServiceProvider::class
 
     ],
@@ -213,7 +212,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Calendar' => MaddHatter\LaravelFullcalendar\Facades\Calendar::class,
+
 
     ],
 
